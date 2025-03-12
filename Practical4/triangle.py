a3a961eb2f4b871c5fbfaceb1582fb65b6830f64
Peng Dayang <dayang.24@intl.zju.edu.cn>
2025-03-12 13:18:43 +0800

    task111

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,3 +1,3 @@
 n=int(input('>')) #input the number of rows
 total_circles=n*(n+1)/2 #senal summing formula
-print(total_circles) #output the total number of circles
+print(total_circes) #output the total number of circles
