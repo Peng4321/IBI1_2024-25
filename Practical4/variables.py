d47f932c7bfdc4ac060357b37a7de2f999b09703
Peng Dayang <dayang.24@intl.zju.edu.cn>
2025-03-12 14:06:58 +0800

    new

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,7 +1,7 @@
 a,b=15,75
-c=a+b
+c=a+b #calculate the total time for bus-based transportation
 d,e=90,5
-f=d+e
+f=d+e #calculate the total time for car-based transportation
 if c>f:
     print('bus-based transportation is faster than car-based transportation')
 elif c<f:
