254bd688ec5302efd2b7026e3c34534ebaf4de9d
Peng Dayang <dayang.24@intl.zju.edu.cn>
2025-03-12 13:49:39 +0800

    graded

--- Practical4/bmi_calcuator.py
+++ Practical4/bmi_calcuator.py
@@ -8,9 +8,9 @@ height=float(input('m:'))
 bmi =  weight/(height**2)
 if bmi<18.5:
     grade='Underweight'
-elif bmi<25 and bmi>=18.5:
+elif bmi<30 and bmi>=18.5:
     grade='Normal weight'
 else:
-    grade='Overweight'
+    grade='obete'
 bmi=round(bmi,2) #round the BMI value to 2 decimal places
-print(f'Your BMI is {bmi} and you are {grade}') #output the BMI value
+print(f'Your BMI is {bmi} and you are at {grade}') #output the BMI value
