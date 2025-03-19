0edc2e212ce4e189124faddd04a74adff5d413b5
Peng Dayang <dayang.24@intl.zju.edu.cn>
2025-03-19 13:50:28 +0800

    practical on class

--- Practical5/language_use.py
+++ Practical5/language_use.py
@@ -3,12 +3,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 language_using={'Javacript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5}
-languages=list(language_using.keys())
-values=list(language_using.values())
-fig,ax=plt.subplots()
-ax.bar(languages,values)
-ax.set_ylabel('Percentage')
-ax.set_title('Percentage of languages used in the world')
+languages=list(language_using.keys())# store the key values of the dictionary in a list
+values=list(language_using.values())# store the values of the dictionary in a list
+plt.title('Percentage of computer languages used in the world')
+plt.xlabel('Languages')
+plt.ylabel('Percentage')
+plt.bar(languages,values,color='green')
+
 plt.show()
 language='JavaScript'#the language type that can be modified(JavaScript,HTML,Python,SQL,TypeScript)
-print(language)
+print(language_using[language])
--- Practical5/population.py
+++ Practical5/population.py
@@ -1,4 +1,18 @@
-import sys
-print(sys.executable)  # 打印 Python 可执行文件的路径
-print(sys.version)  # 打印 Python 版本
-
+# store the population of UK countries
+# store the populations of the neighbouring provinces of Zhejiang
+#Print	two	lists of sorted values using the data above for the populations of countries in the UK and Zhejiang-neighbouring provinces in China
+# make two pie chart of the populations of countries in the UK and Zhejiang-neighbouring provinces in China
+uk_countries=[57.11,3.13,1.91,5.45]
+neighbouring_provinces=[65.77,41.88,45.28,61.27,81.15]
+import matplotlib.pyplot as plt
+import numpy as np
+plt.xlabel('Countries')
+plt.ylabel('Population')
+plt.title('Population of countries in the UK')
+plt.pie(uk_countries,explode=(0.1,0,0,0),labels=['England','Wales','Northern Ireland','Scotland'],autopct='%1.1f%%')
+plt.show()
+plt.xlabel('Provinces')
+plt.ylabel('Population')
+plt.title('Population of neighbouring provinces of Zhejiang')
+plt.pie(neighbouring_provinces,explode=(0,0,0,0.1,0),labels=['zhejiang','Fujian','Jiangxi','Anhui','Jiangsu'],autopct='%1.1f%%')
+plt.show()
