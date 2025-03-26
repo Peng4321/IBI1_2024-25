fb958d5e6976075272d6e0f0b053b4875804bc34
Peng Dayang <dayang.24@intl.zju.edu.cn>
2025-03-26 13:54:37 +0800

    little polish

--- SIR_vaccination.py
+++ SIR_vaccination.py
@@ -4,7 +4,6 @@ from matplotlib.pyplot import cm
 
 # Create the variables and the relative values to start the modal
 N=10000
-I=1
 beta=0.3
 gamma=0.05
 
@@ -16,6 +15,7 @@ for J in np.arange(0.00, 1.10, 0.10):# adjust the population of vaccinated peopl
     I = 1
     R=int(10000*J)#the number of people who have been vaccinated 
     S=9999-R
+    S=max(S,0)
     for i in range(1000):
         new_recovered=np.random.choice(range(2),I, p=[1-gamma,gamma]) #for every infected person, we calculate the probability of recovering
         sum_new_recovered=sum(new_recovered)
