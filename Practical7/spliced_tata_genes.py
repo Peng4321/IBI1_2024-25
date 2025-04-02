6011a9faad4f827dd89caa671261c3ecf60dec5d
Peng Dayang <dayang.24@intl.zju.edu.cn>
2025-04-02 16:02:05 +0800

    modified

---
+++ Practical7/spliced_tata_genes.py
@@ -0,0 +1,42 @@
+#the overall goal of the script is to find the genes that have a specific splice combination and TATA box in their sequence and tell the numbers of TATA boxes in the sequence
+#the program will read the file line by line 
+#if start with >,find the name of the gene
+#if not start with >, add the sequence to the gene_seq variable
+#when meet the next >(means the last gene sequenc has been fully added),check the gene_seq variable for the TATA box and the splice combination
+import re
+
+splice_combination=input("Please enter the splice combination: ") #get the splice combination from the user
+splice_donor=splice_combination[0:2] #get the splice donor from the splice combination
+splice_acceptor=splice_combination[2:4] #get the splice acceptor from the splice combination
+
+tata_pattern = re.compile(r'TATA[AT]A[AT]')
+input_filename = "Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
+output_filename = f"{splice_combination}_spliced_genes.fa"
+
+with open(input_filename, 'r') as fasta_file, open(output_filename, 'w') as out_file:
+    gene_name = None
+    gene_seq = ""
+
+    for line in fasta_file:
+        if line.startswith(">"):  
+            if gene_name and tata_pattern.search(gene_seq): 
+                tata_count = len(tata_pattern.findall(gene_seq))
+                if re.search(splice_donor + r".+" + splice_acceptor, gene_seq):
+                    out_file.write(f">{gene_name} TATA count: {tata_count}\n{gene_seq}\n")
+            
+            gene_name = re.search(r'gene:(\S+)', line)
+            gene_name = gene_name.group(1) 
+            gene_seq = ""  
+        
+        else: 
+            gene_seq += line.strip()
+   
+   
+#to check the last gene
+    if gene_name and tata_pattern.search(gene_seq):
+        tata_count = len(tata_pattern.findall(gene_seq))
+        if re.search(splice_donor + r".+" + splice_acceptor, gene_seq):
+            out_file.write(f">{gene_name} TATA count: {tata_count}\n{gene_seq}\n")
+
+
+
--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -1,8 +1,14 @@
+#the program will read the file line by line 
+#if start with >,find the name of the gene
+#if not start with >, add the sequence to the gene_seq variable
+#when meet the next >(means the last gene sequenc has been fully added),check the gene_seq variable for the TATA box 
+#check the last gene seperately
+import re
 import re
 #to read the sequence and find the gene with TATA box
 #to write the variables to enable the editing of the file
-input_file_path = r"C:\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
-output_file_path = r"C:\Users\彭大扬\OneDrive - International Campus, Zhejiang University\桌面\IBI\IBI1_2024-25\Practical7\tata_genes.fa.txt"
+input_file_path = r"Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
+output_file_path = r"tata_genes.fa"
 line_length = 60
 tata_pattern = re.compile(r'TATA[AT]A[AT]')
 gene_name = ''
@@ -14,12 +20,18 @@ with open(input_file_path, 'r') as input_file:
                 with open(output_file_path, 'a') as output_file:
                     output_file.write(f">{gene_name}\n")
                     for i in range(0, len(gene_seq), line_length):
-                        output_file.write(gene_seq[i:i + line_length] + '\n')
+                        output_file.write(gene_seq[i:i + line_length] + '\n') #output the sequence in lines of 60 characters
             gene_name = re.findall(r'gene:(.+?)\s', line)
             if gene_name:
                 gene_name = gene_name[0] #change the type of gene_name to string
             else:
-                gene_name = ''
+                gene_name=''
             gene_seq = ''
-        else:
-            gene_seq += line.strip() #add the sequence to the gene_seq variable
+        else: 
+            gene_seq += line.strip() #remove the new line character and add the sequence to gene_seq   
+        
+    if gene_seq and tata_pattern.search(gene_seq):
+        output_file.write(f">{gene_name}\n")
+        for i in range(0, len(gene_seq), line_length):
+            output_file.write(gene_seq[i:i + line_length] + '\n')
+ 
