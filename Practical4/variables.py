3d9a339a1feb25fb062e3c7558b2f7b2afd89d0a
Peng Dayang <dayang.24@intl.zju.edu.cn>
2025-04-25 11:31:04 +0800

    change the result

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -3,15 +3,15 @@ c=a+b #calculate the total time for bus-based transportation
 d,e=90,5
 f=d+e #calculate the total time for car-based transportation
 if c>f:
-    print('bus-based transportation is faster than car-based transportation')
-elif c<f:
     print('car-based transportation is faster than bus-based transportation')
+elif c<f:
+    print('bus-based transportation is faster than car-based transportation')
 else:
     print('bus-based transportation is as fast as car-based transportarion')
 
-
+ 
 X=True
-Y=False
+Y=False 
 W=X and Y
 print(W)
 #X=True and Y=False, so W=False
